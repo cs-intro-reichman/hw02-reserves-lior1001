7854f6ce389c9e39efeecabf6b3a28ff271ccf48
Lior Dadonne <Liorgal.dadonne@runi.ac.il>
2024-02-01 17:40:17 +0200

    Commit message

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,7 +5,9 @@ public class DamkaBoard {
 		//go over the columns
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {	//go over the rows
-				System.out.print("* ");
+				System.out.print("*");
+				if((i % 2 == 0) || (j < n-1) )
+					System.out.print(" ");
 			}
 			if(i % 2 == 0) {
 				System.out.println();
